ae6c7c54b3c39b945cace7c0c936b079d73dfec2
Joe B <joeboros72@gmail.com>
2014-12-09 23:33:17 -0500

    initialize repository

---
+++ Final_Exam/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinalExam::Application.config.secret_token = '443e5f8b5a5c3b13af08f038973bbf975de63865607a12f52a2029fbcd729d6a45dd0407467f7102238b1dbd6c10843fbe08de265dc8c97bb889104444fe75b5'
